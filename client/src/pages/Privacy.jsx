9e12bdba667d1ccb8b5d5a464ee94b67064f707b
Darshan G K <122042809+imDarshanGK@users.noreply.github.com>
2025-09-05 02:57:28 +0000

    feat: add Privacy page

---
+++ client/src/pages/Privacy.jsx
@@ -0,0 +1,3 @@
+export default function Privacy() {
+  return <div className="edu-page"><h1>Privacy Policy</h1><p>Our privacy-first promise: We do not sell your data. See details here.</p></div>;
+}
